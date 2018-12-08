a798f45b15d856d5656531cac2c31ab3ec6e93e5
venkat1919 <venkat@ukkoteknik.com>
2018-12-08 22:39:44 +0530

    initial commit

---
+++ app/controller.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sairam
+ * Date: 08/12/18
+ * Time: 8:09 PM
+ */
---
+++ test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sairam on 08/12/18.
+ */
+function sum(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
